c11f08eb759ab2aae5d256474c7baaaa2f2e27a3
Carlosrd97 <80940008+Carlosrd97@users.noreply.github.com>
2021-06-13 21:54:34 -0700

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'Solrac#0680'
+description 'Simple Script to show the weapons on back and waist'
+version '1.0.0'
+
+client_scripts {
+    'client.lua'
+}
